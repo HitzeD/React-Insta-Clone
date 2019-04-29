4f41eb394e1f165d0335b3cbdfe524684d6551b8
HitzeD <d-hitze@comcast.net>
2019-04-29 17:55:10 -0500

    finished page

--- instagram-app/src/postContainer/PostContainer.js
+++ instagram-app/src/postContainer/PostContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CommentSection from '../commentSection/CommentSection';
+import PropTypes from 'prop-types';
 import './postContainer.css'
 
 const PostContainer = props => {
@@ -26,5 +27,25 @@ const PostContainer = props => {
         </>
     );
 }
+
+PostContainer.propTypes = {
+    post: PropTypes.arrayOf(
+        PropTypes.shape({
+            username: PropTypes.string.isRequired,
+            thumbnailUrl: PropTypes.string.isRequired,
+            imageUrl: PropTypes.string.inRequired,
+            likes: PropTypes.number,
+            timestamp: PropTypes.string,
+            comments: PropTypes.arrayOf({
+                username: PropTypes.string,
+                text: PropTypes.string,
+            })
+        }) 
+    ).isRequired
+}
+  
+  PostContainer.defaultProps = {
+    post: []
+  };
 
 export default PostContainer;
